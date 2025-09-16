c2c26f10872ed61b1d28c824ac483074a5f4e41a
Ellias Ribeiro <elliasribeiro99@gmail.com>
2025-09-16 15:07:00 -0300

    feat: add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('To-Do List API')
+    .setDescription('A simple API to manage your to-do lists and tasks')
+    .setVersion('1.0')
+    .addTag('to-do')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
