9bda7e1948d4dddf65cc6e69ff47229765f12657
Ellias Ribeiro <elliasribeiro99@gmail.com>
2025-09-17 19:20:56 -0300

    feat(app): enhance health check response with author information and update task description

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TasksModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [
     AppService,
     {
